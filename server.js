46000ff3f7107023b67efedfa2b5767ad30f6537
Muhammad Hamza Sohail <hamza.sohail29@gmail.com>
2024-07-30 01:13:42 -0400

    Added Database

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const cors = require('cors');
+const credentials = require('./key.json');
+const express = require('express');
 const app = express();
+const admin = require('firebase-admin');
 const PORT = process.env.PORT || 3000;
 
 // Enable CORS for all origins
+admin.initializeApp({
+	credential: admin.credential.cert(credentials)
+});
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+const db = admin.firestore();
 
 // Ensure the data directory exists
 const dataDir = path.join(__dirname, 'data');
@@ -23,35 +29,18 @@ app.get('/', (req, res) => {
 });
 
 // Handle POST requests to /save-data
-app.post('/save-data', (req, res) => {
+app.post('/save-data', async (req, res) => {
     console.log('Received data:', req.body);
     const data = req.body;
 
-    // Check if file exists and if headers need to be written
-    const writeHeaders = !fs.existsSync(filePath);
-    const values = Object.values(data).map(value => `"${value}"`).join(',') + '\n';
-
-    if (writeHeaders) {
-        // Write headers and then data
-        const headers = Object.keys(data).map(key => `"${key}"`).join(',') + '\n';
-        fs.writeFile(filePath, headers + values, (err) => {
-            if (err) {
-                console.error('Error writing to CSV file:', err);
-                return res.status(500).send('Error');
-            }
-            console.log('Data saved successfully');
-            res.send('Success');
-        });
-    } else {
-        // Append data to existing file
-        fs.appendFile(filePath, values, (err) => {
-            if (err) {
-                console.error('Error writing to CSV file:', err);
-                return res.status(500).send('Error');
-            }
-            console.log('Data saved successfully');
-            res.send('Success');
-        });
+    try {
+        // Create a new document with a unique ID in the 'foosball-data' collection
+        await db.collection('foosball-data').add(data);
+        console.log('Data saved successfully');
+        res.send('Success');
+    } catch (error) {
+        console.error('Error saving data to Firestore:', error);
+        res.status(500).send('Error');
     }
 });
 
