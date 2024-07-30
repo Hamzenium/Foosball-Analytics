0112d4575fecd007cc29edb9801b32fc33f13d98
Muhammad Hamza Sohail <hamza.sohail29@gmail.com>
2024-07-30 01:17:29 -0400

    Fixed URL

--- public/script.js
+++ public/script.js
@@ -95,7 +95,7 @@ document.getElementById('export-btn').addEventListener('click', () => {
         'Scored': scoringStatus ? 'Yes' : 'No'
     };
 
-    fetch('http://localhost:3000/save-data', {
+    fetch('https://foosball-ai-c97c13f1b078.herokuapp.com/save-data', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
